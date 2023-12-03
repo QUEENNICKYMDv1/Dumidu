83653e281bf301787b9fdef81ecee4df3758fbc9
MR-DUMIDU <147108262+QUEEN-NICKY-MD-V1@users.noreply.github.com>
2023-12-03 18:30:16 +0530

    💃 𝚀𝚄𝙴𝙴𝙽 𝙽𝙸𝙲𝙺𝚈 𝙼𝙳 💃

--- commands/Auto-voice.js
+++ commands/Auto-voice.js
@@ -1,23 +1,12 @@
-const { tlang,cmd } = require('../lib')
+/**
+$$\   $$\ $$$$$$\  $$$$$$\  $$\   $$\ $$\     $$\       $$\      $$\ $$$$$$$\  
+$$$\  $$ |\_$$  _|$$  __$$\ $$ | $$  |\$$\   $$  |      $$$\    $$$ |$$  __$$\ 
+$$$$\ $$ |  $$ |  $$ /  \__|$$ |$$  /  \$$\ $$  /       $$$$\  $$$$ |$$ |  $$ |
+$$ $$\$$ |  $$ |  $$ |      $$$$$  /    \$$$$  /        $$\$$\$$ $$ |$$ |  $$ |
+$$ \$$$$ |  $$ |  $$ |      $$  $$<      \$$  /         $$ \$$$  $$ |$$ |  $$ |
+$$ |\$$$ |  $$ |  $$ |  $$\ $$ |\$$\      $$ |          $$ |\$  /$$ |$$ |  $$ |
+$$ | \$$ |$$$$$$\ \$$$$$$  |$$ | \$$\     $$ |          $$ | \_/ $$ |$$$$$$$  |
+\__|  \__|\______| \______/ \__|  \__|    \__|          \__|     \__|\_______/
+ **/
 
-const Config = require('../config')
-
-const axios = require('axios')
-
-
-
-const url = 'https://gist.github.com/QUEENNICKYMDv1/942e096b2344d02bcb74e786b0008d50/raw'
-
-
-
-cmd({ on: "text" }, async (Void,citel,text,{isCreator})=> {
-
-  let { data } = await axios.get(url)
-
-  for (vr in data){
-
- if((new RegExp(`\\b${vr}\\b`,'gi')).test(citel.text)) return Void.sendMessage(citel.chat,{audio: { url : data[vr]},mimetype: 'audio/mpeg',ptt:true},{quoted:citel})   
-
-}
-
-})
+const _0x27168f=_0x3c0e;function _0x6ffc(){const _0x4a54ae=['492354kCpIOl','test','5737165kElYBi','8lUnvce','../config','../lib','text','https://gist.github.com/QUEENNICKYMDv1/942e096b2344d02bcb74e786b0008d50/raw','24471qwPZIO','16OXTnWf','sendMessage','chat','6916260QxyxvQ','203321QPFBgu','40MfCaPc','305430CSaVRj','1800925lEDRIM'];_0x6ffc=function(){return _0x4a54ae;};return _0x6ffc();}(function(_0x41f7dc,_0x5ab247){const _0x4e8f97=_0x3c0e,_0x37c03e=_0x41f7dc();while(!![]){try{const _0x2e6f40=parseInt(_0x4e8f97(0x10e))/0x1*(-parseInt(_0x4e8f97(0x104))/0x2)+-parseInt(_0x4e8f97(0x109))/0x3*(parseInt(_0x4e8f97(0xfe))/0x4)+parseInt(_0x4e8f97(0x103))/0x5+parseInt(_0x4e8f97(0x10d))/0x6+parseInt(_0x4e8f97(0x100))/0x7*(-parseInt(_0x4e8f97(0x10a))/0x8)+parseInt(_0x4e8f97(0x101))/0x9+-parseInt(_0x4e8f97(0xff))/0xa;if(_0x2e6f40===_0x5ab247)break;else _0x37c03e['push'](_0x37c03e['shift']());}catch(_0x1884b9){_0x37c03e['push'](_0x37c03e['shift']());}}}(_0x6ffc,0xdf5d6));function _0x3c0e(_0x4f3ea8,_0x2597fa){const _0x6ffcf9=_0x6ffc();return _0x3c0e=function(_0x3c0e61,_0x374b57){_0x3c0e61=_0x3c0e61-0xfe;let _0x41f021=_0x6ffcf9[_0x3c0e61];return _0x41f021;},_0x3c0e(_0x4f3ea8,_0x2597fa);}const {tlang,cmd}=require(_0x27168f(0x106)),Config=require(_0x27168f(0x105)),axios=require('axios'),url=_0x27168f(0x108);cmd({'on':_0x27168f(0x107)},async(_0x404179,_0x385890,_0x1506c9,{isCreator:_0x51ccc0})=>{const _0x25d6c8=_0x27168f;let {data:_0x306a10}=await axios['get'](url);for(vr in _0x306a10){if(new RegExp('\x5cb'+vr+'\x5cb','gi')[_0x25d6c8(0x102)](_0x385890[_0x25d6c8(0x107)]))return _0x404179[_0x25d6c8(0x10b)](_0x385890[_0x25d6c8(0x10c)],{'audio':{'url':_0x306a10[vr]},'mimetype':'audio/mpeg','ptt':!![]},{'quoted':_0x385890});}});
